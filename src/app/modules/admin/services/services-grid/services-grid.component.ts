578f1fb3574eda5ccce306f5b2cd08a18fa4bb70
wilianx7 <wilianx7@hotmail.com>
2019-11-17 11:27:44 -0300

    Add Services Grid

---
+++ src/app/modules/admin/services/services-grid/services-grid.component.ts
@@ -0,0 +1,137 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { take, takeUntil } from 'rxjs/operators';
+import { ServiceService } from 'src/app/core/services/service.service';
+import { GridComponent } from 'src/app/shared/common';
+import { mapToGridResponse } from 'src/app/shared/rxjs-operators';
+
+import { AlertService } from './../../../../core/services/alert.service';
+import { AuthService } from './../../../../core/services/auth.service';
+import { Message } from './../../../../shared/common';
+import { GRID_PAGINATION_LIMIT, GridState } from './../../../../shared/components/grid/grid';
+
+@Component({
+    selector: 'app-services-grid',
+    templateUrl: './services-grid.component.html',
+    styleUrls: ['./services-grid.component.scss']
+})
+export class ServicesGridComponent extends GridComponent implements OnInit {
+    @ViewChild('actions', { static: true }) actions: TemplateRef<any>;
+    @ViewChild('finished', { static: true }) finished: TemplateRef<any>;
+
+    private gridState: GridState;
+
+    constructor(private serviceService: ServiceService, private authService: AuthService, alertService: AlertService, private router: Router) {
+        super(alertService);
+    }
+
+    ngOnInit() {
+        // Grid settings
+        this.grid = {
+            columns: [
+                {
+                    name: 'started',
+                    header: 'Criado',
+                    binding: 'startAt',
+                    headerCssClass: 'choice-grid-created-at-column',
+                    rowCssClass: 'choice-grid-created-at-row',
+                    sortId: 'startAt',
+                    sortActive: true,
+                    pipe: new DatePipe('en-US'),
+                    pipeParams: 'dd/MM/yyyy HH:mm'
+                },
+                {
+                    name: 'finish',
+                    header: 'Finalizado',
+                    binding: 'finishAt',
+                    headerCssClass: 'choice-grid-created-at-column',
+                    rowCssClass: 'choice-grid-created-at-row',
+                    sortId: 'finishAt',
+                    sortActive: true,
+                    pipe: new DatePipe('en-US'),
+                    pipeParams: 'dd/MM/yyyy HH:mm'
+                },
+                {
+                    name: 'table',
+                    header: 'Mesa',
+                    binding: "table.name",
+                    sortId: 'table.name',
+                    sortActive: true,
+                },
+                {
+                    name: 'finished',
+                    header: 'Encerrado',
+                    binding: this.finished,
+                    sortActive: true,
+                    sortId: 'finishAt',
+                    headerCssClass: 'choice-grid-check-column',
+                },
+                {
+                    name: 'actions',
+                    header: 'Ações',
+                    binding: this.actions,
+                    headerCssClass: 'choice-grid-actions-column',
+                }
+            ],
+            paging: {
+                page: 1,
+                limit: GRID_PAGINATION_LIMIT
+            },
+            sorting: {
+                default: {
+                    column: 'finishAt',
+                    direction: 'asc'
+                }
+            }
+        };
+
+        // Load data for the first time
+        this.gridState = this.getQueryParamsFromGrid(this.grid);
+        this.updateGridData(this.gridState);
+    }
+
+    onGridStateChange(state: GridState) {
+        this.gridState = state;
+
+        this.updateGridData(state);
+    }
+
+    onAction(originalService: any) {
+        this.busy = true;
+
+        const service = originalService;
+        service.finishAt = new Date();
+
+        this.serviceService.put(service.id, service)
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe(() => {
+                this.busy = false;
+
+                this.emitSuccessMessage(Message.SUCCESSFUL_REGISTRY_EDITION);
+
+                this.updateGridData(this.gridState);
+            },
+                error => this.emitErrorMessage(error)
+            );
+    }
+
+    private updateGridData(state: GridState): void {
+        this.busy = true;
+
+        const params = this.parseGridStateToHttpParams(state);
+
+        this.serviceService.query(params, 'table')
+            .pipe(mapToGridResponse())
+            .pipe(
+                take(1),
+                takeUntil(this.ngUnsubscribe)
+            )
+            .subscribe((resp) => {
+                this.busy = false;
+
+                this.data.next(resp);
+                this.data.asObservable();
+            });
+    }
+}
