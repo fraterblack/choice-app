73a4af4f4dcc1cf5d82c1c38540abf151241ef7a
wilianx7 <wilianx7@hotmail.com>
2019-11-16 21:17:20 -0300

    Add ProgressBar e métodos necessários

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -36,6 +37,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatTabsModule,
     MatInputModule,
     MatProgressSpinnerModule,
+    MatProgressBarModule,
     MatChipsModule,
     MatSidenavModule,
     MatCheckboxModule,
@@ -64,6 +66,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatTabsModule,
     MatInputModule,
     MatProgressSpinnerModule,
+    MatProgressBarModule,
     MatChipsModule,
     MatSidenavModule,
     MatCheckboxModule,
