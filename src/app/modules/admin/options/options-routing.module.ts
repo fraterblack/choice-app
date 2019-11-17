13590f76436dd9dcd6c2683b04a1aca1c98714c2
wilianx7 <wilianx7@hotmail.com>
2019-11-16 21:18:37 -0300

    Atualização OptionsModule

--- src/app/modules/admin/options/options-routing.module.ts
+++ src/app/modules/admin/options/options-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { OptionsFormComponent } from './options-form/options-form.component';
+import { OptionsGridComponent } from './options-grid/options-grid.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: OptionsGridComponent
+      },
+      {
+        path: 'create',
+        component: OptionsFormComponent
+      },
+      {
+        path: 'update/:id',
+        component: OptionsFormComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
