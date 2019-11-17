b25fc68ee575441bcc955dae5b6d1482b51cc70b
wilianx7 <wilianx7@hotmail.com>
2019-11-17 11:28:01 -0300

    Update Services Module and Routing

--- src/app/modules/admin/services/services-routing.module.ts
+++ src/app/modules/admin/services/services-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ServicesGridComponent } from './services-grid/services-grid.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: ServicesGridComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/admin/services/services.module.ts
+++ src/app/modules/admin/services/services.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ServicesGridComponent } from './services-grid/services-grid.component';
 import { ServicesRoutingModule } from './services-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ServicesGridComponent
+  ],
   imports: [
     CommonModule,
-    ServicesRoutingModule
+    ServicesRoutingModule,
+    SharedModule
   ]
 })
 export class ServicesModule { }
