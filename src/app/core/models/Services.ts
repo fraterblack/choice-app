af7ca68cf8c54236a87e7c46a0436efd8b097925
fraterblack <fraterblack@bck.com.br>
2019-11-18 23:27:41 -0300

    Fix service model naming

--- src/app/core/models/Services.ts
+++ src/app/core/models/Services.ts
@@ -1,7 +1,7 @@
 import { Model } from './model';
 import { Table } from './table.model';
 
-export class Services extends Model {
+export class Service extends Model {
   id: number;
   start_at: Date;
   finish_at: Date;
