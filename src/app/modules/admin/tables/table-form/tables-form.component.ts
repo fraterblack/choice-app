4ec41a4066529679baa6eba5ce2064499c449209
Alisson Schmitz <alissonschmitz2@hotmail.com>
2019-11-21 01:59:31 -0300

    Add qr code in form table

--- src/app/modules/admin/tables/table-form/tables-form.component.ts
+++ src/app/modules/admin/tables/table-form/tables-form.component.ts
@@ -10,83 +10,107 @@ import { FormComponent, Message } from 'src/app/shared/common';
 import { FormHelper } from 'src/app/shared/form-helper';
 
 @Component({
-    selector: 'app-tables-form',
-    templateUrl: './tables-form.component.html',
-    styleUrls: ['./tables-form.component.scss']
+  selector: 'app-tables-form',
+  templateUrl: './tables-form.component.html',
+  styleUrls: ['./tables-form.component.scss']
 })
 export class TablesFormComponent extends FormComponent implements OnInit {
-    formGroup: FormGroup = new FormGroup({
-        name: new FormControl(),
-        active: new FormControl(true)
-    });
+  formGroup: FormGroup = new FormGroup({
+    name: new FormControl(),
+    active: new FormControl(true)
+  });
 
-    modelId: number;
+  isImageLoading = false;
+  imgQrCode: any;
+  modelId: number;
 
-    constructor(
-        alertService: AlertService,
-        private tableService: TableService,
-        private router: Router, route: ActivatedRoute
-    ) {
-        super(alertService);
+  constructor(
+    alertService: AlertService,
+    private tableService: TableService,
+    private router: Router, route: ActivatedRoute
+  ) {
+    super(alertService);
 
-        this.modelId = route.snapshot.params.id;
+    this.modelId = route.snapshot.params.id;
+  }
+
+  ngOnInit() {
+    if (this.modelId) {
+      this.tableService.get(this.modelId)
+        .pipe(takeUntil(this.ngUnsubscribe))
+        .subscribe((res) => {
+          FormHelper.setFormGroupValues(this.formGroup, res);
+        });
+
+      this.isImageLoading = true;
+      this.tableService.getQrCode(this.modelId)
+        .pipe(takeUntil(this.ngUnsubscribe))
+        .subscribe((data) => {
+          this.createImageFromBlob(data);
+          this.isImageLoading = false;
+        }, error => {
+          this.isImageLoading = false;
+          console.log(error);
+        });
+    }
+  }
+
+  onSave(close?: boolean) {
+    if (!this.validateForm(this.formGroup)) {
+      return;
     }
 
-    ngOnInit() {
-        if (this.modelId) {
-            this.tableService.get(this.modelId)
-                .pipe(takeUntil(this.ngUnsubscribe))
-                .subscribe((res) => {
-                    FormHelper.setFormGroupValues(this.formGroup, res);
-                });
-        }
+    return this.save(close);
+  }
+
+  private save(close?: boolean) {
+    const table = new Table();
+    table.deserialize(FormHelper.getValuesFromFormGroup(this.formGroup));
+
+    let action$: Observable<any>;
+
+    if (this.modelId) {
+      action$ = this.tableService.put(this.modelId, table);
+    } else {
+      action$ = this.tableService.post(table);
     }
 
-    onSave(close?: boolean) {
-        if (!this.validateForm(this.formGroup)) {
-            return;
-        }
+    action$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(res => {
+        this.emitSuccessMessage(
+          this.modelId
+            ? Message.SUCCESSFUL_REGISTRY_EDITION
+            : Message.SUCCESSFUL_REGISTRY_INSERTION);
 
-        return this.save(close);
-    }
+        // When save & close
+        if (close) {
+          this.router.navigate([`/admin/tables`]);
 
-    private save(close?: boolean) {
-        const table = new Table();
-        table.deserialize(FormHelper.getValuesFromFormGroup(this.formGroup));
-
-        let action$: Observable<any>;
-
-        if (this.modelId) {
-            action$ = this.tableService.put(this.modelId, table);
+          // When save only
         } else {
-            action$ = this.tableService.post(table);
+          // When is a new registry, redirect to update
+          if (!this.modelId) {
+            this.router.navigate([`/admin/tables/update/${res.id}`]);
+          }
         }
+      },
+        error => this.emitErrorMessage(error)
+      );
+  }
 
-        action$
-            .pipe(takeUntil(this.ngUnsubscribe))
-            .subscribe(res => {
-                this.emitSuccessMessage(
-                    this.modelId
-                        ? Message.SUCCESSFUL_REGISTRY_EDITION
-                        : Message.SUCCESSFUL_REGISTRY_INSERTION);
+  createImageFromBlob(image: Blob) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      this.imgQrCode = reader.result;
+    }, false);
 
-                // When save & close
-                if (close) {
-                    this.router.navigate([`/admin/tables`]);
-
-                    // When save only
-                } else {
-                    // When is a new registry, redirect to update
-                    if (!this.modelId) {
-                        this.router.navigate([`/admin/tables/update/${res.id}`]);
-                    }
-                }
-            },
-                error => this.emitErrorMessage(error)
-            );
+    if (image) {
+      reader.readAsDataURL(image);
     }
+  }
 
-    onCancel() {
-        this.router.navigate([`/admin/tables`]);
-    }
+  onCancel() {
+    this.router.navigate([`/admin/tables`]);
+  }
 }
